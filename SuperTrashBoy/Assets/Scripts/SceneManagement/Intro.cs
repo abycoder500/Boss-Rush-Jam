26a96e264e094944018c02512ba01c59b3362458
heckert-gamedev <unity.github@nostromo.in-berlin.de>
2023-02-05 00:53:22 +0100

    Implement an improved intro sequence Coroutine that works dynamically

--- SuperTrashBoy/Assets/Scripts/SceneManagement/Intro.cs
+++ SuperTrashBoy/Assets/Scripts/SceneManagement/Intro.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -22,18 +23,55 @@ public class Intro : MonoBehaviour
     private TextSizeIncreaser panel1text;
     private TextSizeIncreaser panel2text;
 
-    private void Start() 
+    private void Start()
     {
-        panel1.SetActive(false);
-        panel2.SetActive(false);    
+        //panel1.SetActive(false);
+        //panel2.SetActive(false);
 
         panel1text = panel1.GetComponent<TextSizeIncreaser>();
         panel2text = panel2.GetComponent<TextSizeIncreaser>();
+
+        // Disable all children of the Intro object
+        foreach (Transform t in transform)
+        {
+            t.gameObject.SetActive(false);
+        }
     }
-    
+
     public void Begin(Fader fader)
     {
-        StartCoroutine(IntroSequence(fader));
+        //StartCoroutine(IntroSequence(fader));
+        StartCoroutine(DynamicIntroSequence(fader));
+    }
+
+    private IEnumerator DynamicIntroSequence(Fader fader)
+    {
+        fader.FadeOutImmediate();
+
+        foreach (Transform t in transform)
+        {
+            GameObject thePanel = t.gameObject;
+            NarrationPanel theNarration = t.GetComponent<NarrationPanel>();
+            if (null == theNarration)
+            {
+                continue;
+            }
+            else
+            {
+                thePanel.SetActive(true);
+                TextSizeIncreaser thePanelText = thePanel.GetComponent<TextSizeIncreaser>();
+                thePanelText.BeginIncrease();
+                yield return new WaitForSeconds(theNarration.enterTime);
+                yield return fader.FadeIn(theNarration.fadeInTime);
+                yield return new WaitForSeconds(theNarration.persistanceTime);
+                yield return fader.FadeOut(theNarration.fadeOutTime);
+                thePanelText.ResetState();
+                thePanel.SetActive(false);
+            }
+        }
+
+        yield return new WaitForSeconds(mainMenuTime);
+        yield return fader.FadeIn(mainMenuFadeInTime);
     }
 
     private IEnumerator IntroSequence(Fader fader)
@@ -57,6 +95,5 @@ public class Intro : MonoBehaviour
         panel2.SetActive(false);
         yield return new WaitForSeconds(mainMenuTime);
         yield return fader.FadeIn(mainMenuFadeInTime);
-        
     }
 }
