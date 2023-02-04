bdf2560da27d11d5f1b550ab50344de7571c033d
heckert-gamedev <unity.github@nostromo.in-berlin.de>
2023-02-05 00:51:15 +0100

    Add NarrationPanel as container for the intro panels timings

---
+++ SuperTrashBoy/Assets/Scripts/SceneManagement/NarrationPanel.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NarrationPanel : MonoBehaviour
+{
+    // This should really be done as a Scriptable Object...
+
+    [SerializeField] public float enterTime;
+    [SerializeField] public float fadeInTime;
+    [SerializeField] public float persistanceTime;
+    [SerializeField] public float fadeOutTime;
+
+}
