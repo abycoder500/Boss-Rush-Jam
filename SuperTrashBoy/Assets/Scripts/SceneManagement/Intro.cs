bb527ac8d663b4ba8b0d71783132db26d051b09d
heckert-gamedev <unity.github@nostromo.in-berlin.de>
2023-02-05 01:03:16 +0100

    Cleanup Intro by removing now obsolete code

--- SuperTrashBoy/Assets/Scripts/SceneManagement/Intro.cs
+++ SuperTrashBoy/Assets/Scripts/SceneManagement/Intro.cs
@@ -5,32 +5,12 @@ using UnityEngine;
 
 public class Intro : MonoBehaviour
 {
-    [SerializeField] private GameObject panel1;
-    [SerializeField] private GameObject panel2;
-
-    [SerializeField] private float panel1EnterTime = 1f;
-    [SerializeField] private float panel1FadeInTime = 0.5f;
-    [SerializeField] private float panel1PersistanceTime = 1f;
-    [SerializeField] private float panel1FadeOutTime = 0.5f;
-
-    [SerializeField] private float panel2EnterTime = 1f;
-    [SerializeField] private float panel2FadeInTime = 0.5f;
-    [SerializeField] private float panel2PersistanceTime = 1f;
-    [SerializeField] private float panel2FadeOutTime = 0.5f;
     [SerializeField] private float mainMenuTime = 20f;
     [SerializeField] private float mainMenuFadeInTime = 0.3f;
 
-    private TextSizeIncreaser panel1text;
-    private TextSizeIncreaser panel2text;
 
     private void Start()
     {
-        //panel1.SetActive(false);
-        //panel2.SetActive(false);
-
-        panel1text = panel1.GetComponent<TextSizeIncreaser>();
-        panel2text = panel2.GetComponent<TextSizeIncreaser>();
-
         // Disable all children of the Intro object
         foreach (Transform t in transform)
         {
@@ -40,7 +20,6 @@ public class Intro : MonoBehaviour
 
     public void Begin(Fader fader)
     {
-        //StartCoroutine(IntroSequence(fader));
         StartCoroutine(DynamicIntroSequence(fader));
     }
 
@@ -74,26 +53,4 @@ public class Intro : MonoBehaviour
         yield return fader.FadeIn(mainMenuFadeInTime);
     }
 
-    private IEnumerator IntroSequence(Fader fader)
-    {
-        fader.FadeOutImmediate();
-        yield return new WaitForSeconds(panel1EnterTime);
-        panel1.SetActive(true);
-        panel1text.BeginIncrease();
-        yield return fader.FadeIn(panel1FadeInTime);
-        yield return new WaitForSeconds(panel1PersistanceTime);
-        yield return fader.FadeOut(panel1FadeOutTime);
-        panel1text.ResetState();
-        panel1.SetActive(false);
-        panel2.SetActive(true);
-        panel2text.BeginIncrease();
-        yield return new WaitForSeconds(panel2EnterTime);
-        yield return fader.FadeIn(panel2FadeInTime);
-        yield return new WaitForSeconds(panel2PersistanceTime);
-        yield return fader.FadeOut(panel2FadeOutTime);
-        panel2text.ResetState();
-        panel2.SetActive(false);
-        yield return new WaitForSeconds(mainMenuTime);
-        yield return fader.FadeIn(mainMenuFadeInTime);
-    }
 }
