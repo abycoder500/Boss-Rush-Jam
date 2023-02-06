bba0e6d6fe6ad5272f7e18ade6faa3c03a1949a3
heckert-gamedev <unity.github@nostromo.in-berlin.de>
2023-02-06 23:54:26 +0100

    Add an Outro class which is derived from Intro but starts the credits roll after the fader text panels

---
+++ SuperTrashBoy/Assets/Scripts/SceneManagement/Outro.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Outro : MonoBehaviour
+{
+    [SerializeField] private float mainMenuTime = 20f;
+    [SerializeField] private float mainMenuFadeInTime = 0.3f;
+
+    [Space(1)]
+    [SerializeField] CreditsRoll creditsRoll;
+
+    private void Start()
+    {
+        // Disable all children of the Intro object
+        foreach (Transform t in transform)
+        {
+            t.gameObject.SetActive(false);
+        }
+    }
+
+    public void Begin(Fader fader)
+    {
+        StartCoroutine(DynamicOutroSequence(fader));
+    }
+
+    protected IEnumerator DynamicOutroSequence(Fader fader)
+    {
+        fader.FadeOutImmediate();
+
+        foreach (Transform t in transform)
+        {
+            GameObject thePanel = t.gameObject;
+            NarrationPanel theNarration = t.GetComponent<NarrationPanel>();
+            if (null == theNarration)
+            {
+                continue;
+            }
+            else
+            {
+                thePanel.SetActive(true);
+                TextSizeIncreaser thePanelText = thePanel.GetComponent<TextSizeIncreaser>();
+                thePanelText.BeginIncrease();
+                yield return new WaitForSeconds(theNarration.EnterTime);
+                yield return fader.FadeIn(theNarration.FadeInTime);
+                yield return new WaitForSeconds(theNarration.PersistanceTime);
+                yield return fader.FadeOut(theNarration.FadeOutTime);
+                thePanelText.ResetState();
+                thePanel.SetActive(false);
+            }
+        }
+
+        yield return new WaitForSeconds(mainMenuTime);
+        yield return fader.FadeIn(mainMenuFadeInTime);
+        if (null != creditsRoll)
+        {
+            creditsRoll.gameObject.SetActive(true);
+            creditsRoll.Begin(fader);
+        }
+        else
+        {
+            Invoke(nameof(Start), 1f);
+        }
+    }
+
+
+}
