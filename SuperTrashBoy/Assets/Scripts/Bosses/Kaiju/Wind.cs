a0ed6d7017a9c3e3d95bf99babb1c9140bb63b2f
Robert Cockell <reddragonmakesgames@gmail.com>
2023-02-10 20:46:02 +0000

    Attempts to produce a wind force

--- SuperTrashBoy/Assets/Scripts/Bosses/Kaiju/Wind.cs
+++ SuperTrashBoy/Assets/Scripts/Bosses/Kaiju/Wind.cs
@@ -7,6 +7,8 @@ public class Wind : MonoBehaviour
     private GameObject player;
     private Collider mCollider;
 
+    private bool isKnockingBack = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +20,14 @@ public class Wind : MonoBehaviour
     {
         if (mCollider.bounds.Intersects(player.GetComponent<Collider>().bounds))
         {
-            //Move player
+            Mover move = player.GetComponent<Mover>();
+            move.TakeKnockBack(transform.forward);
+            isKnockingBack = true;
+        }
+        else if (isKnockingBack)
+        {
+            Mover move = player.GetComponent<Mover>();
+            move.Stop();
         }
     }
 
