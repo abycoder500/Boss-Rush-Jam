d112abdede4b3c2bf5083d26a553c19260ee346a
Robert Cockell <reddragonmakesgames@gmail.com>
2023-02-13 16:49:48 +0000

    Added repositioning to Kaiju

--- SuperTrashBoy/Assets/Scripts/Bosses/Kaiju/KaijuController.cs
+++ SuperTrashBoy/Assets/Scripts/Bosses/Kaiju/KaijuController.cs
@@ -34,6 +34,18 @@ public class KaijuController : MonoBehaviour
     public float windHeight = 9.5f;
     private GameObject windInst = null;
 
+    //Reposition variables
+    public float waitMaximum = 5.0f;
+    public float withdrawSpeed = 1.0f;
+    public float returnSpeed = 1.0f;
+    public float distanceFromCenter = 30.0f;
+    public float retreatDistance = 70.0f;
+    private Vector2 targetPos;
+    private float waitTime;
+    private bool movingBack = false;
+    private bool waitingToReturn = false;
+    private bool movingForward = false;
+
     //Variables for flow control
     public float mCurrentAttackTime = 0;
 
@@ -110,8 +122,7 @@ public class KaijuController : MonoBehaviour
                 break;
 
             case states.repositioning:
-                //TODO
-                ChangeState(states.neutral);
+                Reposition();
                 break;
 
             default:
@@ -252,6 +263,58 @@ public class KaijuController : MonoBehaviour
             Destroy(windInst);
             mInAttack = false;
             ChangeState(states.neutral);
+        }
+    }
+
+    private void Reposition()
+    {
+        if (!mInAttack)
+        {
+            //We've hit this point for the first time
+            mInAttack = true;
+            targetPos = Random.insideUnitCircle.normalized;
+
+            LookAtCenter();
+            waitTime = Random.Range(0, waitMaximum);
+            movingBack = true;
+            waitingToReturn = false;
+            movingForward = false;
+        }
+
+        if (movingBack)
+        {
+            transform.position -= transform.forward * withdrawSpeed;
+            if ((new Vector2(ground.transform.position.x, ground.transform.position.z) 
+                - new Vector2 (transform.position.x, transform.position.z)).magnitude > retreatDistance)
+            {
+                movingBack = false;
+                waitingToReturn = true;
+                mCurrentAttackTime = Time.time;
+            }
+        }
+
+        if (waitingToReturn)
+        {
+            if (Time.time > mCurrentAttackTime + waitTime)
+            {
+                Vector2 horizontalPos = new Vector2(ground.transform.position.x, ground.transform.position.z) + targetPos * retreatDistance;
+                transform.position = new Vector3(horizontalPos.x, transform.position.y, horizontalPos.y);
+                LookAtCenter();
+                waitingToReturn = false;
+                movingForward = true;
+            }
+        }
+
+        if (movingForward)
+        {
+            transform.position += transform.forward * returnSpeed;
+            if ((new Vector2(ground.transform.position.x, ground.transform.position.z)
+                - new Vector2(transform.position.x, transform.position.z)).magnitude < distanceFromCenter)
+            {
+                movingForward = false;
+                mInAttack = false;
+                ChangeState(states.neutral);
+            }
         }
     }
 }
